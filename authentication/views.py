616963264feb396f795f94b409f3cb42098d1b04
katzur <zurawska.kasia@gmail.com>
2022-08-07 12:32:56 +0000

    Add login page and function that allow login credentials verification

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,11 @@
 import json
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.models import User
 from validate_email import validate_email
 from django.contrib import messages
+from django.contrib import auth
 
 # Create your views here.
 
@@ -66,6 +67,22 @@ class RegistrationView(View):
 class LoginView(View):
     def get(self, request):
         return render(request, 'authentication/login.html')
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if username and password:
+            user = auth.authenticate(username=username, password=password)
+
+            if user:
+                if user.is_active:
+                    auth.login(request, user)
+                    messages.success(request, 'Welcome, '+user.username+' You are now logged in!')
+                    return redirect('expenses')
+
+            messages.error(request, 'Account does not exist or invalid credentials. Try again')
+            return render(request, 'authentication/login.html')
 
 
 class IndexView(View):
