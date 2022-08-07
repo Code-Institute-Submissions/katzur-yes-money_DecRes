674a08752c621ff6ea3f9f79bf3e990374e86b0c
katzur <zurawska.kasia@gmail.com>
2022-08-07 01:47:00 +0000

    Add index page and styling

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
-from .views import RegistrationView, UsernameValidationView, EmailValidationView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView, LoginView, IndexView
 
 
 urlpatterns = [
     path('register', RegistrationView.as_view(), name="register"),
+    path('login', LoginView.as_view(), name="login"),
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username"),
-    path('validate-email', csrf_exempt(EmailValidationView.as_view()), name="validate_email")
+    path('validate-email', csrf_exempt(EmailValidationView.as_view()), name="validate_email"),
+    path('index', IndexView.as_view(), name="index"),
 ]
--- yesmoney/settings.py
+++ yesmoney/settings.py
@@ -17,6 +17,7 @@ if os.path.isfile("env.py"):
     import env
 from django.contrib import messages
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -144,5 +145,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MESSAGE_TAGS = {
-    messages.ERROR : 'danger'
+    messages.ERROR: 'danger'
 }
