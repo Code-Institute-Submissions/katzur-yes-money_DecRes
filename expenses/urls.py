309f04a8568c0d5d4a667bb7b3bc2961b34696af
katzur <zurawska.kasia@gmail.com>
2022-08-09 21:58:59 +0000

    Add search function for expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = [
@@ -7,4 +8,5 @@ urlpatterns = [
     path('add-expense', views.add_expense, name="add-expense"),
     path('edit-expense/<int:id>', views.expense_edit, name="expense-edit"),
     path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
+    path('search-expenses', csrf_exempt(views.search_expenses), name="search_expenses"),
 ]
