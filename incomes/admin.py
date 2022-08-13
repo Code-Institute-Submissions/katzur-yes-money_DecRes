d9e4250cc7a6a48754cb5d8422f3a731825c55b5
katzur <zurawska.kasia@gmail.com>
2022-08-13 16:18:00 +0000

    Add customization to Admin Panel Incomes

--- incomes/admin.py
+++ incomes/admin.py
@@ -3,7 +3,11 @@ from .models import Income, Source
 
 
 # Register your models here.
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'description', 'owner', 'source', 'date',)
+    search_fields = ('description', 'source', 'date',)
 
+    list_per_page = 5
 
-admin.site.register(Income)
+admin.site.register(Income, IncomeAdmin)
 admin.site.register(Source)
